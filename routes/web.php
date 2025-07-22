33cb6d88583fd62197574ba780b38a38452a34c1
fariszain <risfa050@gmail.com>
2025-07-22 15:52:48 +0700

    Pesan commit yang deskriptif tentang perubahan yang kamu lakukan

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('page.login');
 });
+
+route::get('/dashboard', function () {
+    return view('page.dashboard');
+})->middleware('auth');
+
+route::get('/profile', function () {
+    return view('page.profile');
+})->middleware('auth');
